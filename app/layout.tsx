1e6a4fc38ed7174bc705e8fdc1248c8ee42a3668
“Ibtisam <ibtisam.arif@gmail.com>
2025-06-05 18:17:19 +0200

    feat: clenaup

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,40 +4,40 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+	? `https://${process.env.VERCEL_URL}`
+	: "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+	metadataBase: new URL(defaultUrl),
+	title: "Theosis ",
+	description: "",
 };
 
 const geistSans = Geist({
-  display: "swap",
-  subsets: ["latin"],
+	display: "swap",
+	subsets: ["latin"],
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster />
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex flex-col gap-20 max-w-5xl p-5">{children}</div>
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={geistSans.className} suppressHydrationWarning>
+			<body className="bg-background text-foreground">
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Toaster />
+					<main className="min-h-screen flex flex-col items-center">
+						<div className="flex flex-col gap-20 max-w-5xl p-5">{children}</div>
+					</main>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
